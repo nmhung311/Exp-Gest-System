9b49b1ac15947050244374431ff4e5d841a79e7d
Hung <hung@example.com>
2025-09-23 03:17:15 +0000

    [update]

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,7 @@
 export default /** @type {import('next').NextConfig} */ ({
   reactStrictMode: true,
-  output: 'export',
-  trailingSlash: true,
+  output: process.env.NODE_ENV === 'production' ? 'export' : undefined,
+  trailingSlash: process.env.NODE_ENV === 'production' ? true : false,
   assetPrefix: process.env.NODE_ENV === 'production' ? '' : '',
   images: {
     unoptimized: true,
